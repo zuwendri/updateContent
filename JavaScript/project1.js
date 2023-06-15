6c7fdacebd8f8a03bb1460b85eaae8a402b4e0bc
zuwendri32@gmail.com <zuwendri32@gmail.com>
2023-06-15 09:04:10 +0700

    Fix Bug, Project Nav

--- JavaScript/project1.js
+++ JavaScript/project1.js
@@ -1,29 +1,29 @@
 let btn = document.getElementById('btn');
 let game1 = document.getElementById('Game1');
 
+
 //btn click event//
 //create Game1
 //hide main tag
-btn.addEventListener('click', function (){
+btn.addEventListener('click', function() {
   game1.style.display = 'block';
   let main = document.getElementById('ctn');
   let bg = document.getElementById('bg');
-  
-  bg.style.backgroundColor ='#000'
+
+  bg.style.backgroundColor = '#000'
   main.style.display = 'none';
   game1.style.display = 'flex';
-  
-  if(game1.style.display == 'flex'){
+
+  if (game1.style.display === 'flex') {
     let btn2 = document.getElementById('btn2')
-    console.log('test')
+    console.log(game1.isConnected)
     //btn2 click Event//
     //Hide Game1
     //create main tag
-    btn2.addEventListener('click', function(){
-      game1.style.display ='none'
-      bg.style.backgroundColor ='#fff'
-  main.style.display = 'block';
-      
+    btn2.addEventListener('click', function() {
+      game1.style.display = 'none'
+      bg.style.backgroundColor = '#fff'
+      main.style.display = 'block';
     })
   }
 })
